a9894e399e996c355fa0446268070c46838316cf
Facundo Berges <facundo.h.berges@gmail.com>
2021-02-04 20:41:33 -0300

    Ejercicios 9 y 10 BIS

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,10 +6,26 @@ Debemos lograr tomar el importe por ID, transformarlo a entero (parseInt), luego
 con un aumento del 10% en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	/*
 	let numeroIngresado;
 	let numeroConAumento;
 
 	numeroIngresado=parseInt(document.getElementById("txtIdSueldo").value);
 	numeroConAumento=(numeroIngresado*10/100)+numeroIngresado;
+	document.getElementById("txtIdResultado").value=numeroConAumento;
+
+	09.Bis: Se pide al usuario el %.*/
+
+	let numeroIngresado;
+	let aumento;
+	let numeroConAumento;
+
+	numeroIngresado=parseInt(document.getElementById("txtIdSueldo").value);
+
+	aumento=prompt("Ingrese % a aumentar: ");
+
+	aumento=parseInt(aumento);
+
+	numeroConAumento=(numeroIngresado*aumento/100)+numeroIngresado;
 	document.getElementById("txtIdResultado").value=numeroConAumento;
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -6,12 +6,29 @@ Debemos lograr tomar el importe por ID. Transformarlo a entero (parseInt), luego
 con un Descuento del 25 % en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	let numeroIngresado;
+	/*let numeroIngresado;
 	let numeroConDescuento;
 
 	numeroIngresado=document.getElementById("txtIdImporte").value;
 	numeroIngresado=parseInt(numeroIngresado);
 
 	numeroConDescuento=(numeroIngresado*0.75);
+	document.getElementById("txtIdResultado").value=numeroConDescuento;*/
+
+	// 10. Bis: Se pide al usuario el porcentaje.
+	let numeroIngresado;
+	let descuento;
+	let numeroADescontar;
+	let numeroConDescuento;
+
+	numeroIngresado=parseInt(document.getElementById("txtIdImporte").value);
+
+	descuento=prompt("Ingrese % a descontar: ");
+
+	descuento=parseInt(descuento);
+
+	numeroADescontar=(numeroIngresado*descuento/100);
+	numeroConDescuento=(numeroIngresado-numeroADescontar);
+
 	document.getElementById("txtIdResultado").value=numeroConDescuento;
 }
