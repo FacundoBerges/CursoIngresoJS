9ecf64fbe5a14de251c1784b8cd756f00e58a9d3
Facundo Berges <facundo.h.berges@gmail.com>
2021-02-04 19:50:42 -0300

    Ejercicio 9

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -4,11 +4,12 @@ Facundo Berges
 Ejercicio 8.-
 Debemos lograr tomar los numeros por ID, transformarlos a enteros (parseInt), 
 realizar la operación correcta y mostrar el resto entre el dividendo y el divisor.
-ej.: "El resto es 0 ."*/
+ej.: "El resto es 0."*/
 function SacarResto()
 {
 	let dividendo;
 	let divisor;
+	let restoDivision;
 	
 	dividendo=document.getElementById("txtIdNumeroDividendo").value;
 	divisor=document.getElementById("txtIdNumeroDivisor").value;
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,15 @@
 /*
-Debemos lograr tomar el importe por ID ,
-transformarlo a entero (parseInt), luego
-mostrar el importe con un aumento del 10 %
-en el cuadro de texto "RESULTADO".*/
+Facundo Berges
+
+Ejercicio 09.-
+Debemos lograr tomar el importe por ID, transformarlo a entero (parseInt), luego mostrar el importe
+con un aumento del 10% en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let numeroIngresado;
+	let numeroConAumento;
 
-	alert("ok");
+	numeroIngresado=parseInt(document.getElementById("txtIdSueldo").value);
+	numeroConAumento=(numeroIngresado*10/100)+numeroIngresado;
+	document.getElementById("txtIdResultado").value=numeroConAumento;
 }
