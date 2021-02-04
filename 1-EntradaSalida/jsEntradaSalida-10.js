bfd218b241700fe5419a9e0d8eb83550d5ba3681
Facundo Berges <facundo.h.berges@gmail.com>
2021-02-04 20:09:43 -0300

    Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,17 @@
 /*
-Debemos lograr tomar el importe por ID.
-Transformarlo a entero (parseInt), luego
-mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"*/
+Facundo Berges
+
+EJercicio 10.-
+Debemos lograr tomar el importe por ID. Transformarlo a entero (parseInt), luego mostrar el importe 
+con un Descuento del 25 % en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let numeroIngresado;
+	let numeroConDescuento;
+
+	numeroIngresado=document.getElementById("txtIdImporte").value;
+	numeroIngresado=parseInt(numeroIngresado);
+
+	numeroConDescuento=(numeroIngresado*0.75);
+	document.getElementById("txtIdResultado").value=numeroConDescuento;
 }
