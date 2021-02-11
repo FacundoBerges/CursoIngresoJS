c2fdb29197e787695db3d4a18634a16f43be9669
Facundo Berges <facundo.h.berges@gmail.com>
2021-02-11 20:18:02 -0300

    Ejercicios Switch 05 al 07

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,27 @@
+/*Facundo Berges
+
+Ejercicio 07:
+Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste*/
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
-
+	let destino = document.getElementById("txtIdDestino").value;
+	let mensaje;
+	
+	switch(destino)
+	{
+		case "Bariloche":
+			mensaje="El destino se encuentra al Oeste de Argentina.";
+			break;
+		case "Cataratas":
+			mensaje="El destino se encuentra al Norte de Argentina.";
+			break;
+		case "Mar del plata":
+			mensaje="El destino se encuentra al Este de Argentina.";
+			break;
+		default:
+			mensaje="El destino se encuentra al Sur de Argentina.";
+			break;
+	}
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
